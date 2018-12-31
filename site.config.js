57379ec9eb94b45ab124ba11dcb7e2c1e2e4ccc2
emarchak <erin.marchak@shopify.com>
2018-12-31 11:19:09 -0500

    introduce typography.

--- site.config.js
+++ site.config.js
@@ -1,9 +1,13 @@
-module.exports = {
+const { dev } = require('./dev.config.env');
+
+const config = {
   build: {
     srcPath: './src',
     outputPath: './docs'
   },
   site: {
-    title: 'RJ2019 Template site'
+    title: 'RJ2019 Template site',
   }
 };
+
+module.exports = {...dev, ...config};
